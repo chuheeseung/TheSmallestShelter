615cdfa670e841d23a2c6b45565c52d1327aea8b
heeseungchu <youverygood@naver.com>
2022-07-02 20:01:17 +0900

    listview route

--- front/src/ListviewPage/photo.jsx
+++ front/src/ListviewPage/photo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Photo({ imgUrl, name, age, gender, species }) {
     return (
         <div className="photo-container">
-            <Link to="/register">
+            <Link to="/detail">
                 <img src={imgUrl} alt="대표 사진"/>
             </Link>
             <div className="photo-morahazi" >
