4f0e95f82b7782ba7585b0c1a83768f3fe8744ac
heeseungchu <youverygood@naver.com>
2022-07-02 11:57:52 +0900

    22.07.01 code

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import "antd/dist/antd.min.css";
+
+import { dummy } from './dataDummy';
+import { Layout } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
+
+import Photo from './components/photo';
+import MenuBar  from './components/menuBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <MenuBar className='menu-bar' />
+      <Layout>
+        <Content className="app-container">
+          <div className="list-title">데이터 조회</div>
+          <div className="list-container">
+            {
+              dummy.results.map((item) => {
+                return (
+                  <Photo 
+                    key = {item.id}
+                    photo = {item.photo}
+                    name = {item.name}
+                    age = {item.age}
+                    disease = {item.disease}
+                  />
+                )
+              })
+            }
+          </div>
+        </Content>
+      </Layout>
     </div>
   );
 }
---
+++ front/src/components/menuBar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MenuBar extends Component {
+    render() {
+        return (
+            <div>
+                Menu bar
+            </div>
+        );
+    }
+}
+
+export default MenuBar;
