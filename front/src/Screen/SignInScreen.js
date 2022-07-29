41f42884ae76c747312e06a0ae81f63df6e9c2b5
heeseungchu <youverygood@naver.com>
2022-07-29 17:34:37 +0900

    import 경로 수정

--- front/src/Screen/SignInScreen.js
+++ front/src/Screen/SignInScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from '../SignInPage/Login';
+import Login from '../SignInPage/components/Login';
 import Footer from '../components/Footer';
 
 function SignInScreen() {
